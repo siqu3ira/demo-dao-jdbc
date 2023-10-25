508e60df855c443243bfc8b7b608350ffd789ad0
Maurilio Siqueira <mauriliosiqueira2001@hotmail.com>
2023-10-25 15:34:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
 
         Department dep = new Department(1, "Books");
         Seller sel = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(sel);
     }
 }
