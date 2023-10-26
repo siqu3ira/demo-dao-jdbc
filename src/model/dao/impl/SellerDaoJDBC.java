635ca322743d81496680af726673201a3bdb79b8
Maurilio Siqueira <mauriliosiqueira2001@hotmail.com>
2023-10-26 14:28:22 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -59,8 +59,29 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public void update(Seller dep) {
+    public void update(Seller seller) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(
+                    "UPDATE seller " +
+                        "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                      + "WHERE id = ? ",
+                    Statement.RETURN_GENERATED_KEYS);
 
+            ps.setString(1, seller.getName());
+            ps.setString(2, seller.getEmail());
+            ps.setDate(3, new Date(seller.getBirthDate().getTime()));
+            ps.setDouble(4, seller.getBaseSalary());
+            ps.setInt(5, seller.getDepartment().getId());
+            ps.setInt(6, seller.getId());
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeSatement(ps);
+        }
     }
 
     @Override
